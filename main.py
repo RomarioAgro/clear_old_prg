822ff22375bbed7b8e0708f7104591e6183c2402
Кожин Роман <resume-gl@yandex.ru>
2023-10-12 09:42:12 +0300

    доделал удаление протухших акций

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
 import os
 import re
 import datetime
+import subprocess
 
 
 def clear_calling_func(ipath: str = 'u:\\prg\\test\\', list_promo=None):
     """
     перебор файла _Продажа.prg и исключение протухших акций
+    получаем список строк которые должны остаться в файле
     :param ipath:
     :param list_promo:
     :return:
     """
-    if list_promo is None:
+    if list_promo is None:  #список истекших акций
         list_promo = ['1212', '1501']
     list_promo2 = [x + '()' for x in list_promo]
-    print(list_promo2)
-    selling_list = []
+    new_selling_list = []
     file_calling = '_Продажа.prg'
-    pattern_promo = r'_Акция_\d{1,4}\(\)'
-    pattern_number = r'\d{1,4}\(\)'
+    pattern_number = r'\d{4}\(\)'
     with open(ipath + file_calling, 'r', encoding='cp866') as f_calling:
-        for line in f_calling:
-            selling_list.append(line[0:len(line) - 1])
+        selling_list = f_calling.readlines()
     for elem in selling_list:
         match_s = re.search(pattern_number, elem)
-        if match_s and match_s[0] not in list_promo2:
-            # if match_s[0] not in list_promo2:
-            # print(match_s[0])
-            print(elem)
-    # print(selling_list)
+        new_promo_str = elem
+        if match_s and match_s[0] in list_promo2:
+            new_promo_str = ''
+        if new_promo_str != '':
+            new_selling_list.append(new_promo_str)
+    return new_selling_list
+
+def new_prg_file(path: str = 'u:\\prg\\test\\',
+                 name:str = '_Продажа.prg',
+                 list_string_file=None):
+    """
+    создаем новый файл _Продажа.prg
+    :param path:
+    :param name:
+    :param list_string_file:
+    :return:
+    """
+    if list_string_file is None:
+        list_string_file = []
+    with open(path + name, 'w', encoding='cp866') as new_file:
+        new_file.writelines(list_string_file)
+
 
 def find_ended_func(ipath: str = 'u:\\prg\\test\\'):
     """
@@ -55,13 +71,31 @@ def find_ended_func(ipath: str = 'u:\\prg\\test\\'):
                                 print(filename)
     return promo_list
 
+def del_ended_prg(ipath: str = 'u:\\prg\\test\\', list_promo=None):
+    """
+    функция удаления файлов истекших функций
+    :param ipath:
+    :param list_promo:
+    :return:
+    """
+    procs = []
+    if list_promo is None:  #список истекших акций
+        list_promo = []
+    for elem in list_promo:
+        p = subprocess.Popen('start /B del {1}*{0}*.prg /Q'.format(elem, ipath), encoding='cp1251', shell=True)
+        procs.append(p)
+    for proc in procs:
+        proc.wait()
+        print(proc.returncode)
 
 
 def main():
     path_prg = 'u:\\prg\\test\\'
     promo_list = []
-    # promo_list = find_ended_func(ipath=path_prg)
-    clear_calling_func(ipath=path_prg, list_promo=promo_list)
+    promo_list = find_ended_func(ipath=path_prg)
+    new_calling_promo = clear_calling_func(ipath=path_prg, list_promo=promo_list)
+    new_prg_file(path=path_prg, name='_Продажа.prg', list_string_file=new_calling_promo)
+    del_ended_prg(ipath=path_prg, list_promo=promo_list)
     # print(promo_list)
 
 
